7b2e1049583d1b39844cb237bd5e02c54a7d4809
mariev <mariecv26@gmail.com>
2019-02-26 13:33:47 -0800

    formatting

--- R/RRmpWald.r
+++ R/RRmpWald.r
@@ -69,23 +69,24 @@ RRmpWald <- function(formula = NULL, data = NULL, compare = c('con', 'vac'),
 	#  if entered as vector (x=) it must be ordered by vac/con pairs: c(11,01,10,00)
 	#  if entered as matrix (x=) it must be must be matrix(c(11,01,10,00),2,2,byrow=T)
 	multvec <- NULL
-	if(!is.null(formula) & !is.null(data)){
-        Xx <- .twoby(formula = formula, data = data, compare = compare, affected = affected)
+	if (!is.null(formula) & !is.null(data)) {
+        Xx <- .twoby(formula = formula, data = data, compare = compare, 
+          affected = affected)
         xtable <- Xx$xtable
 		x <- Xx$freqvec
 		multvec <- Xx$multvec
-	} else if(is.matrix(x)){
-		if(!all(dim(x) == 2)){
+	} else if (is.matrix(x)) {
+		if (!all(dim(x) == 2)) {
 			stop('Table dimensions must be 2 x 2\n')
 		} else {
 			xtable <- x
 			x <- c(x)
 		}
-	} else if(is.vector(x)){
-		if(length(x)!=4){
+	} else if (is.vector(x)) {
+		if (length(x) != 4) {
 			stop('Vector length must be 4\n')
 		}
-		xtable <- matrix(x,2,2,byrow=T)
+		xtable <- matrix(x, 2, 2, byrow = TRUE)
 	}
 
 	N <- sum(x)
@@ -100,10 +101,10 @@ RRmpWald <- function(formula = NULL, data = NULL, compare = c('con', 'vac'),
 	varR <- t(gradR) %*% V %*% t(t(gradR))
 	varlogR <- t(gradlogR) %*% V %*% t(t(gradlogR))
 
-	if(tdist){
-		if(is.null(df))	df <- N - 2
+	if (tdist) {
+		if (is.null(df))	df <- N - 2
 	}
-	if(!is.null(df)){
+	if (!is.null(df)) {
 		q <- qt(c(0.5, alpha / 2, 1 - alpha / 2), df)
 		what <- paste(100 * (1 - alpha), "% t intervals on ", df, " df\n", sep = "")
 	} else {
@@ -112,62 +113,64 @@ RRmpWald <- function(formula = NULL, data = NULL, compare = c('con', 'vac'),
 	}
 	
 	ci.dl <- exp(logR + q * sqrt(varlogR))
-	# use the one based on log(R), unless R=0
-	if(R==0){
+	
+	# use the one based on log(R), unless R = 0
+	if (R == 0) {
 		ci.d <- R + q * sqrt(varR) 
 		ci.dl <- ci.d 
 	}
 
 	int <- ci.dl
-    if(!pf) {
+    if (!pf) {
         names(int) <- c("RR", "LL", "UL")
     } else {
         int <- 1 - int[c(1, 3, 2)]
         names(int) <- c("PF", "LL", "UL")
     }
-    
-    # out <- list(estimate = int,  estimator = ifelse(pf, 'PF', 'RR'), compare = 
-		# compare, alpha = alpha, rnd = rnd, xtable = xtable, freqvec = x, 
-		# multvec = multvec)
-   # class(out) <- 'rr1'
-    # return(out)
-	if(is.null(multvec)){
 
-		return(rrmp$new(estimate = int, estimator = ifelse(pf, 'PF', 'RR'), compare = 
-			compare, alpha = alpha, rnd = rnd, xtable = xtable, freqvec = x))
+	if (is.null(multvec)) {
+		return(rrmp$new(estimate = int, estimator = ifelse(pf, 'PF', 'RR'), 
+		  compare = compare, alpha = alpha, rnd = rnd, xtable = xtable, freqvec = x))
 	} else {
 
-		return(rrmp$new(estimate = int, estimator = ifelse(pf, 'PF', 'RR'), compare = 
-			compare, alpha = alpha, rnd = rnd, xtable = xtable, freqvec = x, multvec =
-			multvec))
+		return(rrmp$new(estimate = int, estimator = ifelse(pf, 'PF', 'RR'), 
+		  compare = compare, alpha = alpha, rnd = rnd, xtable = xtable, freqvec = x, 
+		  multvec =	multvec))
 	}
 }
+
+
 .twoby <- function(formula, data, compare, affected){
 	cluster <- function(x) {return(x)}
-    this.call <- match.call()
-		drop.levels <- function (x){
-		for (j in 1:ncol(x)) if (is.factor(x[, j])) 
-			x[, j] <- factor(as.character(x[, j]))
-		return(x)
+  this.call <- match.call()
+	drop.levels <- function(x){
+		for (j in 1:ncol(x)) {
+		  if (is.factor(x[, j])) {
+			  x[, j] <- factor(as.character(x[, j]))
+		  }
 		}
+		return(x)
+	}
 	data <- drop.levels(data)
-    Terms <- terms(formula,specials = 'cluster', data = data)
+  Terms <- terms(formula, specials = 'cluster', data = data)
 	environment(Terms) <- environment()
-    A <- model.frame(formula = Terms, data = data)
-    dat <- A[, 1]
-    group <- A[, 2]
-    clusters <- A[, 3]
-    tbl <- table(clusters, dat, group)[, 2, 1:2]
-	for(i in 1:2){
-		tbl[,i] <- ifelse(tbl[,i]==affected,'af','un')
+  A <- model.frame(formula = Terms, data = data)
+  dat <- A[, 1]
+  group <- A[, 2]
+  clusters <- A[, 3]
+  tbl <- table(clusters, dat, group)[, 2, 1:2]
+	for (i in 1:2) {
+		tbl[,i] <- ifelse(tbl[,i] == affected, 'af', 'un')
 		}
 	tbl <- data.frame(tbl)	
 	# both levels must be present in both groups
-	for(i in 1:2) 
-		levels(tbl[,i]) <- c(levels(tbl[,i]),c('af','un')[!c('af','un') %in% levels(tbl[,i])]) 
-	xtable <- table(tbl[,compare[2]],tbl[,compare[1]])
+	for (i in 1:2) {
+		levels(tbl[,i]) <- c(levels(tbl[,i]), c('af', 'un')[!c('af', 'un') %in% 
+		    levels(tbl[,i])]) 
+	}
+	xtable <- table(tbl[,compare[2]], tbl[,compare[1]])
 	# order table
-	xtable <- xtable[c('af','un'),c('af','un')] 
+	xtable <- xtable[c('af', 'un'), c('af', 'un')] 
 	names(dimnames(xtable)) <- rev(compare)
 	dimnames(xtable) <- lapply(dimnames(xtable), function(x){ifelse(x == 'af', 
 		'pos', 'neg')})
