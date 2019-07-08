40b9f5f5436601cc0b5dbb717a1d9438ee0cc829
mariev <mariecv26@gmail.com>
2019-07-08 16:02:30 -0700

    IDRlsi test cases

---
+++ tests/testthat/test_IDRlsi.R
@@ -0,0 +1,24 @@
+context("IDRlsi")
+
+test_that("examples work", {
+  y1 <- c(26, 204, 10, 205)
+  ex1 <- IDRlsi(y1, pf = FALSE)
+  expect_s4_class(ex1, "rrsi")
+  expect_identical(ex1$estimator, "IDR")
+  expect_equal(ex1$alpha, 0.0414, tolerance = 0.00005)
+  expect_identical(ex1$y, y1)
+  expect_identical(ex1$k, 8)
+  expect_identical(names(ex1$estimate), c("IDR", "LL", "UL"))
+  expect_equal(ex1$estimate %>% unname, c(2.613, 1.265, 5.884),
+    tolerance = 0.00005)
+  
+  y2 <- matrix(c(26, 178, 10, 195), 2, 2, byrow = TRUE)
+  ex2 <- IDRlsi(y2, pf = FALSE)
+  expect_s4_class(ex2, class(ex1))
+  expect_identical(ex2$estimator, ex1$estimator)
+  expect_equal(ex2$alpha, ex1$alpha)
+  expect_identical(ex2$y, ex1$y)
+  expect_identical(ex2$k, ex1$k)
+  expect_identical(names(ex2$estimate), names(ex1$estimate))
+  expect_equal(ex2$estimate %>% unname, ex1$estimate %>% unname)
+})
