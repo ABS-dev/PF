afe8e58fe025a2cfb313d19cb8d7575a2b1d8f01
mariev <mariecv26@gmail.com>
2019-07-09 07:37:18 -0700

    first commit, use examples from help page

---
+++ tests/testthat/test_RRlsi.r
@@ -0,0 +1,21 @@
+context("RRlsi")
+
+test_that("examples work", {
+  y1 <- c(4, 24, 12, 28)
+  ex1 <- RRlsi(y1)
+  
+  expect_s4_class(ex1, "rrsi")
+  expect_identical(ex1$estimator, "PF")  
+  expect_identical(ex1$estimate %>% signif(ex1$rnd) %>% unname,
+    c(0.611, 0.0168, 0.8860))
+  expect_identical(names(ex1$estimate), c("PF", "LL", "UL"))
+  expect_identical(ex1$y, c(4, 24, 12, 28))
+  expect_identical(ex1$k, 8)
+  expect_identical(round((1 - ex1$alpha) * 100, 3), 95.858)
+  
+  y2 <- matrix(c(4, 20, 12, 16), 2, 2, byrow = TRUE)
+  ex2 <- RRlsi(y2)
+  expect_equal(ex1, ex2)
+})
+  
+  
