b6f7fbf8f5aa0a33c850a5735997144b22af8d7e
mariev <mariecv26@gmail.com>
2019-07-09 12:01:47 -0700

    first commit; uses examples from help page

---
+++ tests/testthat/test_tauWt.R
@@ -0,0 +1,19 @@
+context("tauWt")
+
+test_that("examples work", {
+  birdm.fit <- glm(cbind(y, n - y) ~ tx - 1, binomial, birdm)
+  ex1 <- tauWt(birdm.fit)
+  thiscall <-  call("glm", formula = quote(cbind(y, n - y) ~ tx - 1),
+    family = quote(binomial), data = quote(birdm), weights = quote(w),
+    x = quote(T), y = quote(T))
+  
+  expect_identical(ex1$call, thiscall)
+  expect_equal(coefficients(ex1) %>% 
+      unname %>% 
+      signif(3), c(1.030, -0.505))
+  expect_equal(ex1$deviance %>% signif(3), 4.21)
+  expect_equal(ex1$null.deviance %>% signif(3), 7.88)
+  expect_equal(ex1$aic %>% signif(3), 12.7)
+  
+  
+})
