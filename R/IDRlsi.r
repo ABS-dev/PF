6cb1fc3575d7ac592acc3ad3d9d770e2e47e85d3
mariev <mariecv26@gmail.com>
2019-07-11 14:45:47 -0700

    run RStudio code formatting. no additions/deletions of code

--- R/IDRlsi.r
+++ R/IDRlsi.r
@@ -72,75 +72,111 @@
 ## IDRlsi
 ##--------------------------------------- 
 
-IDRlsi <- function(y, alpha = 0.05, k = 8, use.alpha = FALSE, pf = TRUE, converge = 1e-8, 
-	rnd = 3, start = NULL, trace.it = FALSE, iter.max = 24){
-	# support interval based on factoring orthogonal parameterization (Royall, p. 152)
-	# Coded 1999
-	L <- function(y,r){
-		y1 <- y[1]
-		h1 <- y[2]
-		y2 <- y[3]
-		h2 <- y[4]
-		h <- h1/h2
-		Lk <- (r*h)^y1 / (1+r*h)^(y1+y2)
-		return(Lk)
-		}
-	# data vector
-	if(is.matrix(y))
-		y <- c(t(cbind(y[,1],apply(y,1,sum))))	
-	y1 <- y[1]
-	h1 <- y[2]
-	y2 <- y[3]
-	h2 <- y[4]
-	p1 <- y1/h1
-	p2 <- y2/h2
-	r.hat <- p1/p2
-	
-	if(use.alpha)
-		k <- exp(qchisq(1-alpha,1)/2)
-		else alpha <- 1 - pchisq(log(k) * 2, 1)
-	
-	if(is.null(start)) start <- r.hat*(cbind(c(.4,.5),c(1.5,2)))
-	end <- L(y,r.hat)/k
-	si <- rep(NA,2)
-	for(i in 1:2){
-		Q <- c(L(y,start[1,i]),L(y,start[2,i]))
-		Q <- Q[rev(order(abs(Q-end)))]
-		tt <- start[rev(order(abs(Q-end))),i]
-		Q2 <- Q[2]
-		Q1 <- Q[1]
-		t2 <- tt[2]
-		t1 <- tt[1]
-		iter <- 0
-		repeat{
-			iter <- iter+1
-			if(trace.it) cat('iter',iter,'\t')
-			t3 <- t2 +(t2-t1)*(end-Q2)/(Q2-Q1)
-			if(trace.it) cat('t321',t3,t2,t1,'321',L(y,t3),Q2,Q1,'converge',abs(t3-t2)/t2,'\n')
-			if(iter > 1) if(abs((t3-t2)/t2) < converge) break
-			t1 <- t2
-			t2 <- t3
-			Q1 <- Q2
-			Q2 <- L(y,t3)
-			if(iter > iter.max) break
-		}
-		si[i] <- t3
-	}
-	int <- c(r.hat,si)
-	if(!pf) 
-        names(int) <- c("IDR", "LL", "UL")
-    else{
-        int <- 1 - int[c(1,3,2)]
-        names(int) <- c("PF.IDR", "LL", "UL")
+IDRlsi <-
+  function(y,
+    alpha = 0.05,
+    k = 8,
+    use.alpha = FALSE,
+    pf = TRUE,
+    converge = 1e-8,
+    rnd = 3,
+    start = NULL,
+    trace.it = FALSE,
+    iter.max = 24) {
+    # support interval based on factoring orthogonal parameterization (Royall, p. 152)
+    # Coded 1999
+    L <- function(y, r) {
+      y1 <- y[1]
+      h1 <- y[2]
+      y2 <- y[3]
+      h2 <- y[4]
+      h <- h1 / h2
+      Lk <- (r * h) ^ y1 / (1 + r * h) ^ (y1 + y2)
+      return(Lk)
     }
-	
-	y <- as.data.frame(t(y))
-	names(y) <- c("y1", "n1", "y2", "n2")
-	return(rrsi$new(estimate = int, estimator = ifelse(pf, 'PF_IDR', 'IDR'), 
-	  y = y, rnd = rnd, k=k, alpha = alpha))
+    # data vector
+    if (is.matrix(y))
+      y <- c(t(cbind(y[, 1], apply(y, 1, sum))))
+    y1 <- y[1]
+    h1 <- y[2]
+    y2 <- y[3]
+    h2 <- y[4]
+    p1 <- y1 / h1
+    p2 <- y2 / h2
+    r.hat <- p1 / p2
+    
+    if (use.alpha) {
+      k <- exp(qchisq(1 - alpha, 1) / 2)
+    }	else {
+      alpha <- 1 - pchisq(log(k) * 2, 1)
+    }
+    
+    if (is.null(start)) {
+      start <- r.hat * (cbind(c(.4, .5), c(1.5, 2)))
+    }
+    end <- L(y, r.hat) / k
+    si <- rep(NA, 2)
+    for (i in 1:2) {
+      Q <- c(L(y, start[1, i]), L(y, start[2, i]))
+      Q <- Q[rev(order(abs(Q - end)))]
+      tt <- start[rev(order(abs(Q - end))), i]
+      Q2 <- Q[2]
+      Q1 <- Q[1]
+      t2 <- tt[2]
+      t1 <- tt[1]
+      iter <- 0
+      repeat {
+        iter <- iter + 1
+        if (trace.it) {
+          cat('iter', iter, '\t')
+        }
+        t3 <- t2 + (t2 - t1) * (end - Q2) / (Q2 - Q1)
+        if (trace.it) {
+          cat('t321',
+            t3,
+            t2,
+            t1,
+            '321',
+            L(y, t3),
+            Q2,
+            Q1,
+            'converge',
+            abs(t3 - t2) / t2,
+            '\n')
+        }
+        if (iter > 1)
+          if (abs((t3 - t2) / t2) < converge)
+            break
+        t1 <- t2
+        t2 <- t3
+        Q1 <- Q2
+        Q2 <- L(y, t3)
+        if (iter > iter.max)
+          break
+      }
+      si[i] <- t3
+    }
+    int <- c(r.hat, si)
+    if (!pf)
+      names(int) <- c("IDR", "LL", "UL")
+    else{
+      int <- 1 - int[c(1, 3, 2)]
+      names(int) <- c("PF.IDR", "LL", "UL")
+    }
+    
+    y <- as.data.frame(t(y))
+    names(y) <- c("y1", "n1", "y2", "n2")
+    return(rrsi$new(
+      estimate = int,
+      estimator = ifelse(pf, 'PF_IDR', 'IDR'),
+      y = y,
+      rnd = rnd,
+      k = k,
+      alpha = alpha
+    ))
     # out <- list(estimate = int, estimator = ifelse(pf, 'PF_IDR', 'IDR'), y = y, rnd = rnd, k=k, alpha = alpha)
     # class(out) <- 'rrsi'
     # return(out)
-	}
+  }
 
 # idr.si(c(26,204,10,
