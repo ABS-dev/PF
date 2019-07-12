d5d43a01d08e0cf857e439d98ed2d01d0222a8d6
mariev <mariecv26@gmail.com>
2019-07-12 12:22:31 -0700

    add function to convert data+formula to input frequency vector c(y1, n1, y2, n2)
    -1 is assumed to be control group, y- is number of positives, n- is
    total; handle state where stratification group is ignored and counts are
    summarized

--- R/aaa.r
+++ R/aaa.r
@@ -34,3 +34,35 @@
   }
   
 }
+
+
+# Extract frequency vector (y1, n1, y2, n2) where -1 is control group, 
+# y- are positive counts and n- is total count from data+vector input
+# handle both summarized data (each group on one line) and stratified data where 
+# we intend to ignore the strafication and summarize by the control and 
+# vaccinate groups 
+.extract_freqvec <- function(formula, data, compare = c('con', 'vac')) {
+  vars <- all.vars(formula)
+  
+  if (nrow(data) > 2) {
+    message("'y' and 'n' values will be summed by ",
+      paste(compare, sep = '', collapse = ' and '), " designation in data",
+      "column ", vars[3])
+  }
+  sumdata <- data %>% 
+    ungroup %>%
+    select(vars[1:3]) %>%
+    group_by_at(vars[3]) %>%
+    summarize_at(c(vars[1:2]), sum, na.rm = TRUE)
+  colnames(sumdata) <- c('group', 'y', 'n')
+  out <- c(sumdata %>%
+      filter(group == compare[1]) %>%
+      select(y, n) %>%
+      as.numeric,
+    sumdata %>%
+      filter(group == compare[2]) %>%
+      select(y, n) %>%
+      as.numeric)
+  
+  return(out)
+}
