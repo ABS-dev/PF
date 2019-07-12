910d1f82d19e566422b5d9a8e4b1386619623265
mariev <mariecv26@gmail.com>
2019-07-12 13:22:11 -0700

    run RStudio code formatting. no additions/deletions of code

--- R/IDRsc.r
+++ R/IDRsc.r
@@ -1,6 +1,6 @@
 #' @title IDR confidence interval.
 #' @description Estimates confidence interval for the incidence density ratio or prevented fraction based on it.
-#' @details The incidence density is the number of cases per subject-time; its distribution is assumed Poisson. \code{IDRsc} estimates 
+#' @details The incidence density is the number of cases per subject-time; its distribution is assumed Poisson. \code{IDRsc} estimates
 #' a confidence interval for the incidence density ratio using Siev's formula based on the Poisson score statistic.
 #' \eqn{
 #' IDR=\widehat{IDR}\left\{ 1+\left( \frac{1}{{{y}_{1}}}+\frac{1}{{{y}_{2}}} \right)\frac{z_{\alpha /2}^{2}}{2}\ \ \pm \ \ \frac{z_{\alpha
@@ -10,7 +10,7 @@
 #' \code{matrix(c(y1, n1 - y1, y2, n2 - y2), 2, 2, byrow = TRUE)}.
 #' @param y Data vector c(y1, n1, y2, n2) where y are the positives, n are the total, and group 1 is compared to group 2.
 #' @param alpha Complement of the confidence level.
-#' @param pf Estimate \emph{IDR}, or its complement \emph{PF}? 
+#' @param pf Estimate \emph{IDR}, or its complement \emph{PF}?
 #' @param rnd Number of digits for rounding. Affects display only, not estimates.
 #' @return A \code{\link{rr1}} object with the following elements.
 #'  \item{estimate}{vector with point and interval estimate}
@@ -20,66 +20,75 @@
 #'  \item{alpha}{complement of confidence level}
 #' @export
 #' @references Siev D, 1994. Estimating vaccine efficacy in prospective studies. \emph{Preventive Veterinary Medicine} 20:279-296, Appendix 1.
-#' \cr Graham PL, Mengersen K, Morton AP, 2003. Confidence limits for the ratio of two rates based on likelihood scores:non-iterative method 
+#' \cr Graham PL, Mengersen K, Morton AP, 2003. Confidence limits for the ratio of two rates based on likelihood scores:non-iterative method
 #' \emph{Statistics in Medicine} 22:2071-2083.
 #' \cr Siev D, 2004. Letter to the editor. \emph{Statistics in Medicine} 23:693. (Typographical error in formula: replace the two final minus
 #' signs with subscript dots.)
 #' @author David Siev \email{david.siev@@aphis.usda.gov}
 #' @seealso \code{\link{IDRlsi}}
-#' 
+#'
 #' @examples
 #' # Both examples represent the same observation, with data entry by vector
 #' # and matrix notation.
-#' 
+#'
 #' y_vector <- c(26, 204, 10, 205)
 #' IDRsc(y_vector, pf = FALSE)
-#' 
-#' # IDR 
+#'
+#' # IDR
 #' # 95% interval estimates
-#' 
-#' #  IDR   LL   UL 
-#' # 2.61 1.28 5.34 
-#' 
+#'
+#' #  IDR   LL   UL
+#' # 2.61 1.28 5.34
+#'
 #' y_matrix <- matrix(c(26, 178, 10, 195), 2, 2, byrow = TRUE)
 #' y_matrix
 #' #      [,1] [,2]
 #' # [1,]   26  178
 #' # [2,]   10  195
-#' 
+#'
 #' IDRsc(y_matrix, pf = FALSE)
-#' 
-#' # IDR 
+#'
+#' # IDR
 #' # 95% interval estimates
-#' 
-#' #  IDR   LL   UL 
-#' # 2.61 1.28 5.34 
+#'
+#' #  IDR   LL   UL
+#' # 2.61 1.28 5.34
 
 #-------------------------------
 # IDRsc
 #-------------------------------
-IDRsc <- function(y, alpha = 0.05, pf = TRUE, rnd =3){
-	if (is.matrix(y))
-		y <- c(t(cbind(y[,1], apply(y, 1, sum))))
-	y1 <- y[1.]
-	s1 <- y[2.]
-	y2 <- y[3.]
-	s2 <- y[4.]
-	y.dot <- y1 + y2
-	z <- qnorm(1. - alpha/2.)
-	idr.hat <- (y1 * s2)/(y2 * s1)
-	det <- (z * sqrt(y.dot * (y.dot * z^2. + 4. * y1 * y2)))/(2. * y1 * y2)
-	det <- c( -det, det)
-	ci <- idr.hat * (1. + (z^2. * (1./y1 + 1./y2))/2. + det)
-	int <- c(idr.hat, ci)
-	if (!pf) {
+IDRsc <- function(y,
+  alpha = 0.05,
+  pf = TRUE,
+  rnd = 3) {
+  if (is.matrix(y))
+    y <- c(t(cbind(y[, 1], apply(y, 1, sum))))
+  y1 <- y[1.]
+  s1 <- y[2.]
+  y2 <- y[3.]
+  s2 <- y[4.]
+  y.dot <- y1 + y2
+  z <- qnorm(1. - alpha / 2.)
+  idr.hat <- (y1 * s2) / (y2 * s1)
+  det <-
+    (z * sqrt(y.dot * (y.dot * z ^ 2. + 4. * y1 * y2))) / (2. * y1 * y2)
+  det <- c(-det, det)
+  ci <- idr.hat * (1. + (z ^ 2. * (1. / y1 + 1. / y2)) / 2. + det)
+  int <- c(idr.hat, ci)
+  if (!pf) {
     names(int) <- c("IDR", "LL", "UL")
   } else {
     int <- 1 - int[c(1, 3, 2)]
     names(int) <- c("PF.IDR", "LL", "UL")
   }
-	names(y) <- c('y1', 'n1', 'y2', 'n2')
-	
-	return(rr1$new(estimate = int, estimator = ifelse(pf, 'PF_IDR', 'IDR'), 
-	  y = as.data.frame(t(y)), rnd = rnd, alpha = alpha))
-
+  names(y) <- c('y1', 'n1', 'y2', 'n2')
+  
+  return(rr1$new(
+    estimate = int,
+    estimator = ifelse(pf, 'PF_IDR', 'IDR'),
+    y = as.data.frame(t(y)),
+    rnd = rnd,
+    alpha = alpha
+  ))
+  
 }
