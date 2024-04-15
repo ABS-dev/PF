ed755a45670f9eca7ae2baef898c037c0dfed2ed
Thomas Kent <thomas.kent@usda.gov>
2024-04-15 15:00:26 -0500

    Update linter one last time, hopefully...

--- .lint_package.r
+++ .lint_package.r
@@ -13,26 +13,23 @@ linter <- N <- NULL
 # Variables that don't follow stylistic guidelines, which we are not changing.
 okay_vars <- c("A", "B", "C", "L", "N", "Q", "R", "V", "Y", "L1", "L2",
                "Lk", "MN", "Q1", "Q2", "U1", "U2", "Xx", "Y1", "Y2", "Phi",
-               "Tau", "Y$C", "d.i", "m.i", "n.i", "p.i", "u.p", "v.i", "y.i",
-               "z.s", "RRmh", "RRor", "RRsc", "ci.d", "g.ph", "logR", "r.ij",
-               "varR", "wTau", "z.ph", "IDRsc", "RRlsi", "RRstr", "Terms",
-               "V.eta", "ci.dl", "gradR", "p.set", "phiWt", "q.set", "r.hat",
-               "r.max", "r.min", "rsbWt", "tauWt", "var.b", "y.dot", "z.ah2",
-               "z.al2", "z.new", "z.old", "z.phi", "IDRlsi", "Phi.ML",
+               "Tau", "d.i", "m.i", "n.i", "p.i", "u.p", "v.i", "y.i", "z.s",
+               "RRmh", "RRor", "RRsc", "ci.d", "g.ph", "logR", "r.ij", "varR",
+               "wTau", "z.ph", "IDRsc", "RRlsi", "RRstr", "Terms", "V.eta",
+               "ci.dl", "gradR", "p.set", "phiWt", "q.set", "r.hat", "r.max",
+               "r.min", "rsbWt", "tauWt", "var.b", "y.dot", "z.ah2", "z.al2",
+               "z.new", "z.old", "z.phi", "IDRlsi", "Phi.ML", "Table6",
                "V.beta", "ll.max", "mu.hat", "p.test", "rr.est", "rr.opt",
                "scst.y", "zi.phi", "RRotsst", "RRtosst", "df.test", "idr.hat",
                "num.dig", "old.low", "phi.mle", "phi.new", "phi.old",
-               "rho.mle", "tau.hat", "tau.new", "varlogR", "zis.phi", "zsc.phi",
-               "zsk.phi", "RRmpWald", "beta.hat", "bird.fit", "ci.asymp",
-               "fit.only", "gradlogR", "hom.test", "iter.max", "old.high",
-               "rr.ci.hi", "rr.ci.lo", "tauOptim", "trace.it", "Table6$tx",
+               "rho.mle", "tau.hat", "tau.new", "varlogR", "zis.phi",
+               "zsc.phi", "zsk.phi", "RRmpWald", "beta.hat", "bird.fit",
+               "ci.asymp", "fit.only", "gradlogR", "hom.test", "iter.max",
+               "old.high", "rr.ci.hi", "rr.ci.lo", "tauOptim", "trace.it",
                "birdm.fit", "this.call", "use.alpha", "var.log.r", "full.track",
-               "grad.log.r", "p.set$n1y1", "p.set$n2y2", "q.set$n1y1",
-               "q.set$n2y2", "show.warns", "var.log.rr", "drop.levels",
+               "grad.log.r", "show.warns", "var.log.rr", "drop.levels",
                "score.start", "options.warn", "var.beta.hat", "show.warnings",
                "newfamily.name", ".rr.score.asymp", "nuisance.points")
-
-okay_vars <- c()
 
 ## Change This Value ##
 # Determine whose files should be linted.  Change the number in the [.]
